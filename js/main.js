a8196bd228463df6842a41ac675816d60ec62fc6
Aashika Parekh <47467036+aashikaParekh1@users.noreply.github.com>
2023-12-03 20:08:01 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,141 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+    
+})(jQuery);
+
+//Ending Output
+function displayCap() {
+  var text = document.getElementById("capone");
+  text.style.display = "block";
+
+}
+
+function displayRum() {
+  var text = document.getElementById("rum");
+  text.style.display = "block";
+
+}
+
+function displayBoot() {
+  var text = document.getElementById("boot");
+  text.style.display = "block";
+
+}
+
+function displayCreate() {
+  var text = document.getElementById("create");
+  text.style.display = "block";
+
+}
+
+function displaySoda() {
+  var text = document.getElementById("soda");
+  text.style.display = "block";
+
+}
+
+function displayBlack() {
+  var text = document.getElementById("black");
+  text.style.display = "block";
+
+}
+
+function displayGreen() {
+  var text = document.getElementById("green");
+  text.style.display = "block";
+
+}
+
+function displayOolong() {
+  var text = document.getElementById("oolong");
+  text.style.display = "block";
+
+}
+
+function displayHibi() {
+  var text = document.getElementById("hibi");
+  text.style.display = "block";
+
+}
+
+function displayLychee() {
+  var text = document.getElementById("lychee");
+  text.style.display = "block";
+
+}
+
+function displayRaspberry() {
+  var text = document.getElementById("raspberry");
+  text.style.display = "block";
+
+}
+
+function displayLemonade() {
+  var text = document.getElementById("lemonade");
+  text.style.display = "block";
+
+}
+
+function displayLime() {
+  var text = document.getElementById("lime");
+  text.style.display = "block";
+
+}
+
+function displayOrange() {
+  var text = document.getElementById("orange");
+  text.style.display = "block";
+
+}
+
+function displayMango() {
+  var text = document.getElementById("mango");
+  text.style.display = "block";
+
+}
+
+function displayPom() {
+  var text = document.getElementById("pom");
+  text.style.display = "block";
+
+}
+
+function displayCitrus() {
+  var text = document.getElementById("citrus");
+  text.style.display = "block";
+
+}
+
+function displayMint() {
+  var text = document.getElementById("mint");
+  text.style.display = "block";
+
+}
+
+function displayCherry() {
+  var text = document.getElementById("cherry");
+  text.style.display = "block";
+
+}
+
+function displayConfirm() {
+  var text = document.getElementById("confirm");
+  text.style.display = "block";
+
+}
+
